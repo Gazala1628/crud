8da0fe495e3da0d506020f9a7ff3ba80c08c8654
sohail <patelkhan175@gmail.com>
2024-06-05 15:22:38 +0530

    gazala: crud overall setup and pushed in github

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD in Laravel</title>
+    @include('boot')
+    <style>
+        .nav {
+            font-size: 22px;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Insert</a></li>
+            <li><a href="#">View</a></li>
+          </ul>
+        </div>
+    </nav>
+</body>
+</html>
+
